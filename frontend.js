b9e830265fd670e63ed811cfe04902d6677884af
siwalt1 <siwalt1@t-online.de>
2020-12-03 09:52:57 +0100

    removed ul

--- frontend.js
+++ frontend.js
@@ -11,9 +11,13 @@ $(function () {
     } else {
       user = $('#un').val();
       document.getElementById('login').remove();
-      const msgUl = document.createElement('ul');
-      msgUl.id = 'messages';
-      document.body.appendChild(msgUl);
+      const chat = document.createElement('div');
+      chat.style.padding = '10px';
+      chat.style.marginBottom = '50px';
+      chat.id = 'messages';
+      //const msgUl = document.createElement('ul');
+      //msgUl.id = 'messages';
+      document.body.appendChild(chat);
       const msgForm = document.createElement('form');
       msgForm.id = 'msg';
       const msgInput = document.createElement('input');
@@ -29,8 +33,9 @@ $(function () {
       $('#msg').submit(function (e) {
         e.preventDefault(); // prevents page reloading
         socket.emit('chat message', $('#m').val(), user);
-        $('#messages').append($('<li>').text('me: ' + $('#m').val()));
+        $('#messages').append($('<div>').text('me: ' + $('#m').val()));
         document.getElementById('messages').lastChild.style.textAlign = 'right';
+        document.getElementById('messages').lastChild.scrollIntoView();
         $('#m').val('');
         return false;
       });
@@ -38,27 +43,30 @@ $(function () {
   })
 
   socket.on('chat message', function (msg, uName) {
-    $('#messages').append($('<li>').text(uName + ': ' + msg));
+    $('#messages').append($('<div>').text(uName + ': ' + msg));
+    document.getElementById('messages').lastChild.scrollIntoView();
   });
   socket.on('login message', function (text) {
-    $('#messages').append($('<li class="bold">').text(text));
+    $('#messages').append($('<div class="bold">').text(text));
     document.getElementById('messages').lastChild.style.textAlign = 'center';
+    document.getElementById('messages').lastChild.scrollIntoView();
   })
   socket.on('init msg', function (messages) {
     for (let i = 0; i < messages.length; i++) {
       msg = messages[i];
       if (msg.username === 'System') {
-        $('#messages').append($('<li class="bold">').text(msg.message));
+        $('#messages').append($('<div class="bold">').text(msg.message));
         document.getElementById('messages').lastChild.style.textAlign = 'center';
       } else {
-        $('#messages').append($('<li>').text(msg.username + ': ' + msg.message));
+        $('#messages').append($('<div>').text(msg.username + ': ' + msg.message));
         if (user === msg.username) {
           document.getElementById('messages').lastChild.style.textAlign = 'right';
         }
       }
     }
-    $('#messages').append($('<li class="bold">').text(user + ' logged in'));
+    $('#messages').append($('<div class="bold">').text(user + ' logged in'));
     document.getElementById('messages').lastChild.style.textAlign = 'center';
+    document.getElementById('messages').lastChild.scrollIntoView();
   })
   socket.on('onlineUser', function (conClients) {
     console.log(conClients); //Anzeige aller angemeldeten Clients
